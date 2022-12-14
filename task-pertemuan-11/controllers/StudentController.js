659c7992ffe34495f08816bc3d66e22bfdf6c3a5
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-12-14 18:59:54 +0700

    basic CRUD

--- task-pertemuan-11/controllers/StudentController.js
+++ task-pertemuan-11/controllers/StudentController.js
@@ -1,5 +1,6 @@
 // TODO 3: Import data students dari folder data/students.js
 // code here
+const students = require('../data/students');
 
 // Membuat Class StudentController
 class StudentController {
@@ -8,7 +9,7 @@ class StudentController {
     // code here
     const data = {
       message: "Menampilkkan semua students",
-      data: [],
+      data: [students],
     };
 
     res.json(data);
@@ -19,9 +20,11 @@ class StudentController {
 
     // TODO 5: Tambahkan data students
     // code here
+    students.push(nama);
+
     const data = {
       message: `Menambahkan data student: ${nama}`,
-      data: [],
+      data: [students],
     };
 
     res.json(data);
@@ -33,9 +36,11 @@ class StudentController {
 
     // TODO 6: Update data students
     // code here
+    students.splice(id, 1, nama);
+
     const data = {
       message: `Mengedit student id ${id}, nama ${nama}`,
-      data: [],
+      data: [students],
     };
 
     res.json(data);
@@ -46,9 +51,11 @@ class StudentController {
 
     // TODO 7: Hapus data students
     // code here
+    students.splice(id, 1);
+
     const data = {
       message: `Menghapus student id ${id}`,
-      data: [],
+      data: [students],
     };
 
     res.json(data);
