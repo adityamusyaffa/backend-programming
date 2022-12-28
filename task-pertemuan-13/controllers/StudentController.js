17f07875928c1f8b726baf29771f53f0bcc89496
adityamusyaffa <aditmusyaffa7@gmail.com>
2022-12-28 18:11:41 +0700

    Do task pertemuan 13

--- task-pertemuan-13/controllers/StudentController.js
+++ task-pertemuan-13/controllers/StudentController.js
@@ -7,52 +7,116 @@ class StudentController {
     // memanggil method static all dengan async await.
     const students = await Student.all();
 
+    if (students.length > 0) {
+      const data = {
+        message: "Menampilkkan semua students",
+        data: students,
+      };
+      return res.json(data);
+    }
     const data = {
-      message: "Menampilkkan semua students",
-      data: students,
+      message: "Data students masih kosong",
     };
+    return res.status(404).json(data);
+  }
 
-    res.json(data);
+  async show(req, res) {
+    const { id } = req.params;
+    const student = await Student.find(id);
+
+    if (student) {
+      const data = {
+        message: `Menampilkan detail student dengan id ${id}`,
+        data: student
+      }
+      return res.json(data);
+    }
+
+    const data = {
+      message: `Student with id ${id} doesn't exist`,
+    }
+    return res.status(404).json(data);
   }
 
   async store(req, res) {
-    /**
-     * TODO 2: memanggil method create.
-     * Method create mengembalikan data yang baru diinsert.
-     * Mengembalikan response dalam bentuk json.
-     */
     const { nama, nim, email, jurusan } = req.body;
-    const newStudent = await Student.create(nama, nim, email, jurusan);
 
+    if (!nama || !nim || !email || !jurusan) {
+      const data = {
+        message: "Gagal menambahkan data student, semua data harus dikirim",
+        data: isNaN(nim),
+      };
+      return res.status(422).json(data);
+    }
+
+    if (isNaN(nim)) {
+      const data = {
+        message: "Gagal menambahkan data student, nim harus berupa angka",
+        data: isNaN(nim),
+      };
+      return res.status(422).json(data);
+    }
+
+    const newStudent = await Student.create(req.body);
     const data = {
       message: "Menambahkan data student",
       data: [newStudent],
     };
-    
-    res.status(201).json(data);
+    return res.status(201).json(data);
   }
 
-  update(req, res) {
+  async update(req, res) {
     const { id } = req.params;
-    const { nama } = req.body;
+    const { nama, nim, email, jurusan } = req.body;
+    const student = await Student.find(id);
+
+    if (student) {
+      if (!nama || !nim || !email || !jurusan) {
+        const data = {
+          message: "Gagal mengedit data student, semua data harus dikirim",
+          data: isNaN(nim),
+        };
+        return res.status(422).json(data);
+      }
+
+      if (isNaN(nim)) {
+        const data = {
+          message: "Gagal mengedit data student, nim harus berupa angka",
+          data: isNaN(nim),
+        };
+        return res.status(422).json(data);
+      }
+
+      const student = await Student.update(req.body, id);
+      const data = {
+        message: `Mengedit student id ${id}`,
+        data: student,
+      };
+      return res.json(data);
+    }
 
     const data = {
-      message: `Mengedit student id ${id}, nama ${nama}`,
-      data: [],
-    };
-
-    res.json(data);
+      message: `Data dengan id ${id} tidak ditemukan`,
+    }
+    return res.status(404).json(data);
   }
 
-  destroy(req, res) {
+  async destroy(req, res) {
     const { id } = req.params;
+    const student = await Student.find(id);
 
+    if (student) {
+      await Student.delete(id);
+      const data = {
+        message: `Menghapus student id ${id}`,
+        data: student,
+      };
+      return res.json(data);
+    }
     const data = {
-      message: `Menghapus student id ${id}`,
-      data: [],
-    };
-
-    res.json(data);
+      message: `Data dengan id ${id} tidak ditemukan`,
+    }
+    return res.status(404).json(data);
   }
 }
 
--- task-pertemuan-13/models/Student.js
+++ task-pertemuan-13/models/Student.js
@@ -20,19 +20,45 @@ class Student {
     });
   }
 
-  /**
-   * TODO 1: Buat fungsi untuk insert data.
-   * Method menerima parameter data yang akan diinsert.
-   * Method mengembalikan data student yang baru diinsert.
-   */
-  static create(nama, nim, email, jurusan) {
+  static find(id) {
     return new Promise((resolve, reject) => {
-      const created_at = new Date().toTimeString();
-      const updated_at = created_at;  
-      const sql = `INSERT INTO students VALUES ('', '${nama}', '${nim}', '${email}', '${jurusan}', now(), now());`;
+      const sql = `SELECT * FROM students WHERE id = ?`;
+      db.query(sql, id, (err, results) => {
+        const [student] = results;
+        resolve(student);
+      });
+    });
+  }
 
-      db.query(sql, (err, results) => {
-        resolve(results);
+  static async create(data) {
+    const id = await new Promise((resolve, reject) => {
+      const created_at = "now()";
+      const sql = `INSERT INTO students SET ?, created_at = ${created_at}, updated_at = ${created_at}`;
+      db.query(sql, data, (err, results) => {
+        resolve(results.insertId);
+      });
+    });
+
+    return await Student.find(id);
+  }
+
+  static async update(data, id) {
+    await new Promise((resolve, reject) => {
+      const updated_at = "now()"
+      const sql = `UPDATE students SET ?, updated_at = ${updated_at}  WHERE id = ?`;
+      db.query(sql, [data, id], (err, result) => {
+        resolve(result);
+      });
+    });
+
+    return await this.find(id);
+  }
+
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      const sql = `DELETE FROM students WHERE id = ?`;
+      db.query(sql, id, (err, result) => {
+        resolve(result);
       });
     });
   }
